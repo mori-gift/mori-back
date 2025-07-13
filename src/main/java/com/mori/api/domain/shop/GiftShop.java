6ee5c17f82c9e391425e0ba593217f156acb3b9a
jihwankim128 <jihwan1535@nate.com>
2025-07-13 15:03:48 +0900

    feat(#1): gift shop entity design

---
+++ src/main/java/com/mori/api/domain/shop/GiftShop.java
@@ -0,0 +1,24 @@
+package com.mori.api.domain.shop;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class GiftShop {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private String mainName;
+	private String subName;
+	private String description;
+
+	private String roadAddress;
+	private String detailAddress;
+	private String zipCode;
+
+	private String phoneNumber;
+}
