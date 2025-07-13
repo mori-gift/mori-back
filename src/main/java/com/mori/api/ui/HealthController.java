107c65364d2d9e23b0805a3bafb1e0e8986c91b3
jihwankim128 <jihwan1535@nate.com>
2025-07-13 20:02:17 +0900

    chore(#1): yml 분리

---
+++ src/main/java/com/mori/api/ui/HealthController.java
@@ -0,0 +1,13 @@
+package com.mori.api.ui;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+	@GetMapping
+	public String health() {
+		return "Hello World";
+	}
+}
