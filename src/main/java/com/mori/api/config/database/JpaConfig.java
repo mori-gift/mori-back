2fc1822ec7336162412d716c54bd52bccc0c34ac
jihwankim128 <jihwan1535@nate.com>
2025-07-14 01:27:45 +0900

    chore(#1): container 환경 세팅

--- src/main/java/com/mori/api/config/database/JpaConfig.java
+++ src/main/java/com/mori/api/config/database/JpaConfig.java
@@ -1,31 +1,9 @@
 package com.mori.api.config.database;
 
-import java.time.Clock;
-import java.time.LocalDateTime;
-import java.util.Optional;
-
-import org.hibernate.boot.model.naming.PhysicalNamingStrategy;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.auditing.DateTimeProvider;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-import com.example.green.infra.database.strategy.UpperSnakeNamingStrategy;
-
-@EnableJpaAuditing(
-	auditorAwareRef = "auditorAwareConfig",
-	dateTimeProviderRef = "koreaDateTimeProvider"
-)
+@EnableJpaAuditing
 @Configuration
 public class JpaConfig {
-
-	@Bean
-	public PhysicalNamingStrategy physicalNamingStrategy() {
-		return new UpperSnakeNamingStrategy();
-	}
-
-	@Bean
-	public DateTimeProvider koreaDateTimeProvider(Clock clock) {
-		return () -> Optional.of(LocalDateTime.now(clock));
-	}
 }
