c05d67642b1ca33f743010ec6d52c7e7c66a44fc
jihwankim128 <jihwan1535@nate.com>
2025-07-15 05:28:12 +0900

    feat(#1): api common template

---
+++ src/main/java/com/mori/api/common/ui/dto/ApiTemplate.java
@@ -0,0 +1,24 @@
+package com.mori.api.common.ui.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@EqualsAndHashCode(callSuper = true)
+public class ApiTemplate<T> extends NoContent {
+
+	@Schema(description = "API 응답 결과")
+	private T result;
+
+	private ApiTemplate(boolean success, String message, T result) {
+		super(success, message);
+		this.result = result;
+	}
+
+	public static <T> ApiTemplate<T> ok(String responseMessage, T result) {
+		return new ApiTemplate<>(true, responseMessage, result);
+	}
+}
---
+++ src/main/java/com/mori/api/common/ui/dto/NoContent.java
@@ -0,0 +1,22 @@
+package com.mori.api.common.ui.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Schema(description = "API 공통 응답")
+public class NoContent {
+
+	@Schema(description = "API 성공 여부")
+	private boolean success;
+	@Schema(description = "API 응답 메세지")
+	private String message;
+
+	public static NoContent ok(String message) {
+		return new NoContent(true, message);
+	}
+}
